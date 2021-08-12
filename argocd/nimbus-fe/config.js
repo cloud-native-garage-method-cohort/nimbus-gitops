1b57423d974b68109eae2931344fb459c484c894
Naomi Lin <supakorn.t@ibm.com>
2021-08-12 13:51:35 +0700

    ADD ConfigMap from File

---
+++ argocd/nimbus-fe/config.js
@@ -0,0 +1,3 @@
+window.REACT_APP_BFF_SERVICE_URL='https://web-bff-nimbus-kompare-dev.thtechgarage-4667f5c54a9fa16798873d0072267a42-0000.sng01.containers.appdomain.cloud'
+window.REACT_APP_MOCK='https://redhat.com'
+window.REACT_APP_MARKETIING_API='https://adobe.com'
